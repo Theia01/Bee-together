4d85411e9d6117dac9c7eed0d2bd60cae67f1009
maeldebon <mael.dbn@gmail.com>
2020-07-01 17:01:17 +0200

    Add logout

--- app/Logout.js
+++ app/Logout.js
@@ -1,3 +1,15 @@
 module.exports = class Logout {
+    constructor(session) {
+        Logout.logout(session)
+    }
 
-}
+    static logout(session) {
+        if (session.logged === true) {
+            return true;
+            // déconnecter l'utilisateur ici
+        } else {
+            throw ("Vous n'êtes pas connecté.");
+            // renvoyer un message d'erreur : l'utilisateur n'est pas connecté
+        }
+    }
+};
--- test/LogoutTest.js
+++ test/LogoutTest.js
@@ -1,1 +1,26 @@
-const assert = require('assert')
+const assert = require('assert');
+const Logout = require('../app/Logout');
+
+let logout = null;
+describe("Logout", () => {
+    describe("#logout", () => {
+        it("User is connected, must logout", () => {
+            const session = {"logged": true};
+
+            let result = Logout.logout(session);
+
+            assert.strictEqual(result, true)
+        });
+
+        it("User is not connected, must return an error", () => {
+            const session = {"logged": false};
+
+            try {
+                Logout.logout(session);
+            }
+            catch (e) {
+                assert.equal(e, "Vous n'êtes pas connecté.")
+            }
+        });
+    })
+});
