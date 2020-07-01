b806c496025bcc363490bd9c6bbb78fcd6525700
Johan DELEON <johan@doowup.fr>
2020-07-01 16:04:07 +0200

    Add test setMail (RegisterTest)

--- app/Register.js
+++ app/Register.js
@@ -4,7 +4,7 @@ module.exports = class Register {
 
     }
 
-    setMail() {
+    setMail(email) {
         return null
     }
 }
--- test/RegisterTest.js
+++ test/RegisterTest.js
@@ -1,1 +1,54 @@
 const assert = require('assert')
+const Register = require('../app/Register')
+
+const data = ['pastèque','pastèque@malotrue','pas steak@malotru.com','pasteque@.com', '@steak.com']
+let register = null;
+describe("Register", () => {
+
+    describe("#setMail", () => {
+        beforeEach(() => {
+            register = new Register
+        })
+
+        data.forEach(element => {
+            it("Doit retourner false", () => {
+                // Arrange
+                let result = null
+    
+                // Act
+                result = register.setMail(element)
+    
+                // Assert
+                assert.strictEqual(result,false)
+            })
+        });
+
+        it("Doit retourner false (mail déjà utilisé)", () => {
+            // Arrange
+            const mail = "johdel390@gmail.com"
+            register.usedMails = ["noidea@laposte.net","johdel390@gmail.com","pasteque@hotmail.fr"]
+            let result = null
+
+            // Act
+            result = register.setMail(mail)
+
+            // Assert
+            assert.strictEqual(result,false)
+        })
+
+        it("Doit retourner true", () => {
+            // Arrange
+            const mail = "johdel390@gmail.com"
+            register.usedMails = ["noidea@laposte.net","johdel44390@gmail.com","pasteque@hotmail.fr"]
+            let result = null
+
+            // Act
+            result = register.setMail(mail)
+
+            // Assert
+            assert.strictEqual(result,true)
+        })
+
+
+    })
+})
