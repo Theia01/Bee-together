4486102eac585f98b4e0570e480519e19306eabc
axelle <guinaudeau.ion1516@gmail.com>
2020-07-02 09:59:03 +0200

    #LeDeveloppeurDebile

--- app/Register.js
+++ app/Register.js
@@ -5,11 +5,17 @@ module.exports = class Register {
     }
 
     setMail(email) {
-        return null
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(re.test(String(email).toLowerCase())){
+            if(this.usedMails.indexOf(email) < 0){
+                return true;
+            }
+        }
+        return false
     }
 
     setPassword(password) {
-        return null
+        return false
     }
 
 }
