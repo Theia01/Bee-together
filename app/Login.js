a05bcc34b6687022339b73409905d439beab63eb
maeldebon <mael.dbn@gmail.com>
2020-07-02 11:28:40 +0200

    Add login function

--- app/Login.js
+++ app/Login.js
@@ -1,8 +1,22 @@
 module.exports = class Login {
-    constructor() {
+    constructor() {}
 
-    }
-    login(mail, mpd) {
-        return null
+    login(mail, mdp) {
+        const users = [
+            {"mail": "test@tester.fr", "password": "1234aBc."},
+            {"mail": "test@test.com", "password": "1234aBc."}
+        ]
+
+        console.log(mail)
+
+        if (mail.length > 0 && mdp.length > 0) { // test 1 & 2
+            if (users.some(i => i.mail.includes(mail))) { // test 3
+                let pos = users.map(function(e) {
+                        return e.mail
+                    }).indexOf(mail);
+                return users[pos].password === mdp;
+
+            } else {return false}
+        } else {return false}
     }
 }
