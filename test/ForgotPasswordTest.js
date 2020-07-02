8f2be375a56c11b918fefdaf76716df481683f2b
Lucas Maillard <46894925+LucasMaillard@users.noreply.github.com>
2020-07-02 10:20:05 +0200

    ForgotPassword factorisé

--- test/ForgotPasswordTest.js
+++ test/ForgotPasswordTest.js
@@ -1,20 +1,19 @@
 const assert = require('assert')
 const ForgotPassword = require('../app/ForgotPassword')
 
-const data = ['lucas.maillard@imie.fr','camille.naulet@hotmail.fr','johdel390@gmail.com', 'jeanmichdu92@outcook.de']
 let forgotPassword = null;
 describe("forgotPassword", () => {
 
     describe("#verifMail", () => {
         beforeEach(() => {
             forgotPassword = new ForgotPassword
+            forgotPassword.mails = ["lucas.maillard@imie.fr","camille.naulet@hotmail.fr","johdel390@gmail.com"]
+            let result = null
         })
 
         it("true : l'email existe", () => {
             // Arrange
             const mail = "lucas.maillard@imie.fr"
-            forgotPassword.mails = ["lucas.maillard@imie.fr","camille.naulet@hotmail.fr","johdel390@gmail.com"]
-            let result = null
 
             // Act
             result = forgotPassword.verifMail(mail)
@@ -26,8 +25,6 @@ describe("forgotPassword", () => {
         it("false : Le compte (l'email) n'existe pas", () => {
             // Arrange
             const mail = "jeanmichdu92@outcook.de"
-            forgotPassword.mails = ["lucas.maillard@imie.fr","camille.naulet@hotmail.fr","johdel390@gmail.com"]
-            let result = null
 
             // Act
             result = forgotPassword.verifMail(mail)
