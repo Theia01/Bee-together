8be167363f15c6ee7fde601a78c58341be679a21
Lucas Maillard <46894925+LucasMaillard@users.noreply.github.com>
2020-07-02 17:00:24 +0200

    Réglage de tous les problème sur UpdateProfil.js

--- app/UpdateProfil.js
+++ app/UpdateProfil.js
@@ -6,28 +6,43 @@ module.exports = class UpdateProfil {
 
     updatePseudo(pseudo){
         if(pseudo !== undefined){
-                return true; //prêt à changer
+            if(this.usedLogin.indexOf(pseudo) < 0){
+                return true;
             }
-    
+            }
+            else{
+                throw new Error
+            }
         return false // La case pseudo n'est pas remplie
     }
 
-    updateMail(mail){
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(re.test(String(email).toLowerCase())){
+    updateMail(email) {
+        if(email !== undefined){
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(re.test(String(email).toLowerCase())){
             if(this.usedMails.indexOf(email) < 0){
-                return true;
+                    return true;
+                }
             }
+        }
+        else {
+            throw new Error
         }
         return false
     }
 
 
     updatePassword(password) {
-        const re = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,30})$/
-        if(re.test(password)){
+        if (password !== undefined){
+
+            const re = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,30})$/
+            if(re.test(password)){
                 return true;
         }
+        }
+    else {
+        throw new Error
+    }
         return false
-
+    }
 }
