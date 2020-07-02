adb9fb13e3a4ea7686849ec4b71a9201c7cb290d
Johan DELEON <johan@doowup.fr>
2020-07-02 17:02:31 +0200

    CONFIG DB

--- app/services/DBService.js
+++ app/services/DBService.js
@@ -1,9 +1,14 @@
+require("../../config.database")
+
 module.exports = class DBService {
+    _database = new DatabaseConfig
+
     constructor() {
 
     }
 
     insert() {
+        this._database.initConnection()
         return null
     }
 }
