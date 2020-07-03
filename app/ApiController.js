e03b3027cf7845c0ab5370747de1d2b0c8a4d8c3
Lucas Maillard <46894925+LucasMaillard@users.noreply.github.com>
2020-07-03 11:43:42 +0200

    Mise en place : ApiController.js et Collection.js

---
+++ app/ApiController.js
@@ -0,0 +1,14 @@
+module.exports = class ApiController {
+    /**
+     * URL /api/collection
+     */
+    all() { }
+    /**
+     * URL /api/collection/?store=DATA
+     */
+    store() { }
+     /**
+     * URL /api/collection/?search=DATA
+     */
+    search() { }
+}
