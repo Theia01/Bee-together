9082afaed39a789e10bc77d35caaa15a4dbfb720
camillenaulet03 <camille.naulet@hotmail.fr>
2020-07-03 14:18:41 +0200

    fonction update

--- app/services/DBService.js
+++ app/services/DBService.js
@@ -4,8 +4,8 @@ const { resolve } = require("path")
 module.exports = class DBService {
     _database = new DatabaseConfig
 
-    constructor() {
-
+    constructor(data) {
+        this.data = data
     }
 
     delete(id) {
@@ -69,6 +69,21 @@ module.exports = class DBService {
     }
 
     update(id, data) {
-        return null
+        return new Promise((resolve,reject) => {
+            const db = this._database.initConnection()
+            db.connect()
+            db.query(
+                `UPDATE ${this._database.table.auth.table_name} SET 
+                ${this._database.table.auth.columns.login.name} = ${data['login']}, 
+                ${this._database.table.auth.columns.mail.name} = ${data['mail']}, 
+                ${this._database.table.auth.columns.password.name} = ${data['password']} WHERE ${this._database.table.auth.columns.id.name}=?`, [id], function (error, results, fields) {
+                    db.end()
+                    if(error){
+                        resolve(false)
+                    } else {
+                        resolve(true);
+                    }
+                });
+        })
     }
 }
